1e003621eeb7fbbeb1bda523734d80643efedcd4
Veneta Ivanova <i.veneta.v@gmail.com>
2022-07-17 23:03:52 +0300

    Create 01.printCharacters.js

---
+++ Text Processing/01.printCharacters.js
@@ -0,0 +1,18 @@
+function printCharacters(word) {
+    for (const char of word) {
+        console.log(char);
+    }
+
+}
+//---test---//
+printCharacters('Sentence');
+/*---result---
+S
+e
+n
+t
+e
+n
+c
+e
+ */
