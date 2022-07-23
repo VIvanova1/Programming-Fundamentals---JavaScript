18b4bad7439ad62a731c78f14ec24bd2cb5ea319
Veneta Ivanova <i.veneta.v@gmail.com>
2022-07-23 12:41:44 +0300

    Update 03.thePianist.js

--- Programming Fundamentals Exam/03.thePianist.js
+++ Programming Fundamentals Exam/03.thePianist.js
@@ -39,9 +39,15 @@ function thePianist(data) {
         }
     }
 
+    for (const [key, value] of Object.entries(pieceLab)) {
+        //"{Piece} -> Composer: {composer}, Key: {key}"
+        console.log(`${key} -> Composer: ${value[0]}, Key: ${value[1]}`);
+    }
+
     function adding(piece, composer, key) {
         if (!pieceLab.hasOwnProperty(piece)) {
             pieceLab[piece] = {};
+            console.log(`${piece} by ${composer} in ${key} added to the collection!`);
         } else {
             console.log(`${piece} is already in the collection!`);
         }
@@ -53,37 +59,35 @@ function thePianist(data) {
 
     function removing(piece) {
         if (pieceLab.hasOwnProperty(piece)) {
-            //DELETE
+            delete pieceLab[piece]
             console.log(`Successfully removed ${piece}!`);
         } else {
             console.log(`Invalid operation! ${piece} does not exist in the collection.`);
         }
     }
 
-    function changing(piece, newKey)
-    if (pieceLab.hasOwnProperty(piece)) {
-        pieceLab[piece] = newKey;
-        console.log(`Changed the key of ${piece} to ${new key}!`);
-    } else {
-        console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+    function changing(piece, newKey) {
+        if (pieceLab.hasOwnProperty(piece)) {
+            pieceLab[piece] = newKey;
+            console.log(`Changed the key of ${piece} to ${newKey}!`);
+        } else {
+            console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+        }
     }
-
 }
 //---test 1 ---//
-thePianist(
-    [
-        '3',
-        'Fur Elise|Beethoven|A Minor',
-        'Moonlight Sonata|Beethoven|C# Minor',
-        'Clair de Lune|Debussy|C# Minor',
-        'Add|Sonata No.2|Chopin|B Minor',
-        'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
-        'Add|Fur Elise|Beethoven|C# Minor',
-        'Remove|Clair de Lune',
-        'ChangeKey|Moonlight Sonata|C# Major',
-        'Stop'
-    ]
-);
+thePianist([
+    '3',
+    'Fur Elise|Beethoven|A Minor',
+    'Moonlight Sonata|Beethoven|C# Minor',
+    'Clair de Lune|Debussy|C# Minor',
+    'Add|Sonata No.2|Chopin|B Minor',
+    'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
+    'Add|Fur Elise|Beethoven|C# Minor',
+    'Remove|Clair de Lune',
+    'ChangeKey|Moonlight Sonata|C# Major',
+    'Stop'
+]);
 /*---result---
 Sonata No.2 by Chopin in B Minor added to the collection!
 Hungarian Rhapsody No.2 by Liszt in C# Minor added to the collection!
